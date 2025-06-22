5b0d2852826a073c3e4ad9c3b2ce8fab5437a7e2
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2025-06-22 10:08:36 +0530

    add async handler function

---
+++ server/src/utils/ApiError.ts
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+  statusCode: number;
+  success: boolean;
+  errors: string[];
+  constructor(
+    statusCode: number,
+    message = "Something went wrong",
+    errorCode: number,
+    errors: string[] = [],
+    stack = ""
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, ApiError.prototype);
+    this.statusCode = statusCode;
+    this.success = false;
+    this.errors = errors.length > 0 ? errors : [message];
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
